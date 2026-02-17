a5c90f75431e8e2f8357fd9675f00f045c36d715
chowdary062000 <2101510041mba@gmail.com>
2026-02-17 18:21:37 +0530

    Removed node_modules and updated config files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/AS-COLLECTIONS/',   // important for GitHub Pages
   plugins: [react()],
   server: { host: true }
 })
