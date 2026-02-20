49b402c2101f76dd748db8f3136bb88617ba9d35
thundertheif <144991228+thundertheif@users.noreply.github.com>
2026-02-20 19:51:36 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 // Pages
 import Home from "./pages/Home";
 import Sarees from "./pages/Sarees";
-import Tops from "./pages/tops";
+import tops from "./pages/Tops";
 import Kurtis from "./pages/Kurtis";
 import Cart from "./pages/Cart";
 import DesignerMaterials from "./pages/DesignerMaterials";
